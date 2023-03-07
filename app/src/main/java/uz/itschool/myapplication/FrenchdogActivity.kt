5d38197b9d3465cb7ead018bd413e407a584213c
PC673 <m>
2023-03-07 11:57:12 +0500

    Initial commit

---
+++ app/src/main/java/uz/itschool/myapplication/FrenchdogActivity.kt
@@ -0,0 +1,11 @@
+package uz.itschool.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrenchdogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frenchdog)
+    }
+}
---
+++ app/src/main/java/uz/itschool/myapplication/ItemTypeFoodActivity.kt
@@ -0,0 +1,11 @@
+package uz.itschool.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemTypeFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_type_food)
+    }
+}
